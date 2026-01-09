2787583b1c297d7970c3311d1b358300b6400f0a
akbar <akbaralihussain@gmail.com>
2026-01-08 21:26:48 -0800

    Added base path mapping for API Gateway

--- apig.tf
+++ apig.tf
@@ -110,15 +110,14 @@ resource "aws_api_gateway_stage" "prod" {
   stage_name    = "prod_${var.api_version}"
 }
 
-# resource "aws_api_gateway_base_path_mapping" "prod" {
-#   count = (local.create_custom_domain) ? 1 : 0
+resource "aws_api_gateway_base_path_mapping" "prod" {
+  count = (local.create_custom_domain) ? 1 : 0
 
-#   api_id      = aws_api_gateway_rest_api.main.id
-#   stage_name  = aws_api_gateway_stage.prod.stage_name
-#   domain_name = aws_api_gateway_domain_name.api[0].domain_name
-#   base_path   = var.api_version
-# }
-
+  api_id      = aws_api_gateway_rest_api.main.id
+  stage_name  = aws_api_gateway_stage.prod.stage_name
+  domain_name = aws_api_gateway_domain_name.api[0].domain_name
+  base_path   = var.api_version
+}
 
 # count         = (var.cognito_user_pool_arns != null && length(var.cognito_user_pool_arns) > 0) ? 1 : 0
 resource "aws_api_gateway_authorizer" "cognito" {
